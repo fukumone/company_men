12ee998a5719d8fcdee25c04ef7563a462ebbba0
fukumone <fukui.development@gmail.com>
2018-05-03 07:38:16 +0900

    employee create and update

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -9,6 +9,14 @@ class EmployeesController < ApplicationController
   end
 
   def create
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      flash.notice = '社員の作成に成功'
+      redirect_to root_path
+    else
+      flash.now[:alert] = '社員の作成に失敗'
+      render 'new'
+    end
   end
 
   def edit
@@ -16,6 +24,15 @@ class EmployeesController < ApplicationController
   end
 
   def update
+    @employee = Employee.find(params[:id])
+    @employee.assign_attributes(employee_params)
+    if @employee.save
+      flash.notice = '社員の更新に成功'
+      redirect_to root_path
+    else
+      flash.now[:alert] = '社員の更新に失敗'
+      render 'edit'
+    end
   end
 
   def destroy
@@ -24,5 +41,6 @@ class EmployeesController < ApplicationController
   private
 
   def employee_params
+    params.require(:employee).permit(:first_name, :last_name)
   end
 end
