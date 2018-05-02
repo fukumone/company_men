8ea65a1cf996ec8a82c96f0c6b6c5a6e9c3e4842
fukumone <fukui.development@gmail.com>
2018-05-03 06:56:01 +0900

    gemfile && seed修正

--- db/seeds/employee.rb
+++ db/seeds/employee.rb
@@ -1,39 +1,39 @@
-Employee.create({ first_name: "山田",
+Employee.find_or_create_by({ first_name: "山田",
                   last_name: "太郎",
                   slack_user_id: SecureRandom.hex(6),
                   slack_user_name: 'test.tarou1' })
 
-Employee.create({ first_name: "",
+Employee.find_or_create_by({ first_name: "",
                   last_name: "",
                   slack_user_id: SecureRandom.hex(6),
                   slack_user_name: 'test.tarou2' })
 
-Employee.create({ first_name: "木村",
+Employee.find_or_create_by({ first_name: "木村",
                   last_name: "太郎",
                   slack_user_id: SecureRandom.hex(6),
                   slack_user_name: 'test.tarou3' })
 
-Employee.create({ first_name: "森",
+Employee.find_or_create_by({ first_name: "森",
                   last_name: "太郎",
                   slack_user_id: SecureRandom.hex(6),
                   slack_user_name: 'test.tarou4' })
 
-Employee.create({ first_name: "山岡",
+Employee.find_or_create_by({ first_name: "山岡",
                   last_name: "太郎",
                   slack_user_id: SecureRandom.hex(6),
                   slack_user_name: 'test.tarou5' })
 
-Employee.create({ first_name: "三田",
+Employee.find_or_create_by({ first_name: "三田",
                   last_name: "太郎",
                   slack_user_id: SecureRandom.hex(6),
                   slack_user_name: 'test.tarou6' })
 
-Employee.create({ first_name: "原田",
+Employee.find_or_create_by({ first_name: "原田",
                   last_name: "太郎",
                   slack_user_id: SecureRandom.hex(6),
                   slack_user_name: 'test.tarou7' })
 
-Employee.create({ first_name: "",
+Employee.find_or_create_by({ first_name: "",
                   last_name: "",
                   slack_user_id: SecureRandom.hex(6),
                   slack_user_name: 'test.tarou8' })
