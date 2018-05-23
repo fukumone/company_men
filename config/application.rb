93eb1ca763833d58de97c6a41cc22f97d97c1a1a
fukumone <fukui.development@gmail.com>
2018-05-23 20:36:06 +0900

    locale ja対応

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module CompanyMen
     config.time_zone = 'Tokyo'
 
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [ :ja ]
   end
 end
